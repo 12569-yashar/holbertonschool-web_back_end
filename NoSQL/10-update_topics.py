824b91f835ea5f1e9958d45796d72c1c7a5b9efa
root <you@example.com>
2025-11-29 19:54:22 +0000

    sagray

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """salam"""
 
-
 def update_topics(mongo_collection, name, topics):
-"""ssssssssss"""
-   mongo_collection.update_many(
+    """salam"""
+    mongo_collection.update_many(
         { "name": name },
         { "$set": { "topics": topics } }
     )
